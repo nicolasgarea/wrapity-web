907083fd5ae12741c89ff4b3cb0943b18d17c312
nicolasgarea <nicogareacidre@gmail.com>
2026-04-13 09:45:47 +0200

    feat: add auth service interceptor and guard

---
+++ src/app/core/guards/auth-guard.ts
@@ -0,0 +1,12 @@
+import { CanActivateFn } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const token = localStorage.getItem('access_token');
+
+  if (token) {
+    return true;
+  }
+
+  return router.createUrlTree(['/auth/login']);
+};

c2c0401ae5b1c5a2a3cb7a64eef37ee117658d25
Nicolás Garea <nicogareacidre@gmail.com>
2026-04-13 09:46:32 +0200

    feat: add auth service interceptor and guard (#2)

---
+++ src/app/features/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { UserRegister } from '../../core/models/userRegister';
+import { Observable } from 'rxjs';
+import { UserResponse } from '../../core/models/userResponse';
+import { UserLogin } from '../../core/models/userLogin';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  private API_URL = environment.apiUrl;
+
+  register(data: UserRegister): Observable<UserResponse> {
+    return this.http.post<UserResponse>(`${this.API_URL}/auth/register`, data);
+  }
+
+  login(data: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>(`${this.API_URL}/auth/login`, data);
+  }
+}

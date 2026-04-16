5e541bc15594fcafecefae3c33f316c52d1a1f5f
nicolasgarea <nicogareacidre@gmail.com>
2026-04-16 14:47:35 +0200

    feat: add login form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Register } from './features/auth/register/register';
+import { Login } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: [
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'auth/login',
+  },
+];
---
+++ src/app/features/auth/login/login.ts
@@ -0,0 +1,44 @@
+import { Component, inject, signal } from '@angular/core';
+import { UserLogin } from '../../../core/models/userLogin';
+import { email, form, FormField, required, submit } from '@angular/forms/signals';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormField],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  private authService = inject(AuthService);
+
+  private router = inject(Router);
+
+  loginModel = signal<UserLogin>({
+    email: '',
+    password: '',
+  });
+
+  loginForm = form(this.loginModel, (login) => {
+    required(login.email, { message: 'Email is required' });
+    email(login.email, { message: 'Enter a valid email address' });
+    required(login.password, { message: 'Password is required' });
+  });
+
+  errorMessage = signal('');
+
+  onSubmit(): void {
+    submit(this.loginForm, async () => {
+      this.authService.login(this.loginModel()).subscribe({
+        next: (token) => {
+          localStorage.setItem('access_token', token.access_token);
+          this.router.navigate(['/']);
+        },
+        error: () => {
+          this.errorMessage.set('Invalid email or password');
+        },
+      });
+    });
+  }
+}

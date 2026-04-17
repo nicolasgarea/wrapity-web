4e7762fbefab4fd63335e67e9c2212777928e22b
Nicolás Garea <nicogareacidre@gmail.com>
2026-04-17 10:37:51 +0200

    feat: add public guard and auth redirects (#8)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,30 @@ import { Register } from './features/auth/register/register';
 import { Login } from './features/auth/login/login';
 import { Home } from './features/home/home';
 import { Layout } from './layout/layout';
+import { authGuard } from './core/guards/auth-guard';
+import { publicGuard } from './core/guards/public-guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
+    canActivate: [publicGuard],
     children: [
       { path: 'login', component: Login },
       { path: 'register', component: Register },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
     ],
   },
   {
     path: '',
     component: Layout,
-    children: [{ path: 'home', component: Home }],
+    canActivate: [authGuard],
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: Home },
+    ],
   },
   {
     path: '**',
-    redirectTo: 'auth/login',
+    redirectTo: '',
   },
 ];

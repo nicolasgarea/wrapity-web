6f8fea62d7055f1ce2a1f4402bfae06a6d978038
Nicolás Garea <nicogareacidre@gmail.com>
2026-04-17 10:25:28 +0200

    feat: add user service (#6)

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { UserResponse } from '../models/userResponse';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private http = inject(HttpClient);
+  private API_URL = environment.apiUrl;
+
+  getMe(): Observable<UserResponse> {
+    return this.http.get<UserResponse>(`${this.API_URL}/users/me`);
+  }
+}

79b7d085449b6f3579454125c1856196ec53889f
nicolasgarea <nicogareacidre@gmail.com>
2026-04-17 10:59:11 +0200

    feat: add mobile header

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../shared/navbar/navbar';
 import { RouterOutlet } from '@angular/router';
+import { MobileHeader } from '../shared/mobile-header/mobile-header';
 
 @Component({
   selector: 'app-layout',
-  imports: [Navbar, RouterOutlet],
+  imports: [Navbar, RouterOutlet, MobileHeader],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })

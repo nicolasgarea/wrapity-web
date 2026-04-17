342b3eef7c9b95eb4c3cc74559e4776861c85184
Nicolás Garea <nicogareacidre@gmail.com>
2026-04-17 10:59:47 +0200

    feat: add mobile header (#9)

---
+++ src/app/shared/mobile-header/mobile-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-header',
+  imports: [],
+  templateUrl: './mobile-header.html',
+  styleUrl: './mobile-header.scss',
+})
+export class MobileHeader {}

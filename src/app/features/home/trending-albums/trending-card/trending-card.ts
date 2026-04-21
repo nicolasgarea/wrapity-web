5539f6c90374885c2f262c8fb7ca988c04b9a5a8
nicolasgarea <nicogareacidre@gmail.com>
2026-04-21 14:28:30 +0200

    feat: make album cards navigate to detail

--- src/app/features/home/trending-albums/trending-card/trending-card.ts
+++ src/app/features/home/trending-albums/trending-card/trending-card.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Album } from '../../../../core/models/model/album';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-trending-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './trending-card.html',
   styleUrl: './trending-card.scss',
 })

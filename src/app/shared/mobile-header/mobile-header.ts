c3da32a4c92ff87a9807169615c4be150e358df6
nicolasgarea <nicogareacidre@gmail.com>
2026-04-22 11:30:43 +0200

    feat: integrate logo into mobile header

--- src/app/shared/mobile-header/mobile-header.ts
+++ src/app/shared/mobile-header/mobile-header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Logo } from '../logo/logo';
 
 @Component({
   selector: 'app-mobile-header',
-  imports: [],
+  imports: [Logo],
   templateUrl: './mobile-header.html',
   styleUrl: './mobile-header.scss',
 })

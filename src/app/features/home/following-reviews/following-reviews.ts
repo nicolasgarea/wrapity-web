3087adbe3d4d0ed7394f4cfd531259bbaf9a8ea9
Nicolás Garea <nicogareacidre@gmail.com>
2026-05-05 13:44:31 +0200

    feat: implement reviews (#22)

--- src/app/features/home/following-reviews/following-reviews.ts
+++ src/app/features/home/following-reviews/following-reviews.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FollowingReviewsService } from './following-reviews.service';
+import { ReviewCard } from './review-card/review-card';
+import { Carousel } from '../../../shared/carousel/carousel';
 
 @Component({
   selector: 'app-following-reviews',
-  imports: [],
+  imports: [ReviewCard, Carousel],
   templateUrl: './following-reviews.html',
   styleUrl: './following-reviews.scss',
 })
-export class FollowingReviews {}
+export class FollowingReviews {
+  readonly followingService = inject(FollowingReviewsService);
+
+  followingReviews = toSignal(this.followingService.getFollowingReviews());
+
+  ngOnInit() {
+    console.log(this.followingReviews);
+  }
+}

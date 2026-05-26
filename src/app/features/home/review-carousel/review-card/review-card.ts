02807f2d72a78bb3e9d0d679976e03e60762e327
nicolasgarea <nicogareacidre@gmail.com>
2026-05-26 13:47:53 +0200

    refactor(review-card): emit reviewClick alongside albumClick

--- src/app/features/home/review-carousel/review-card/review-card.ts
+++ src/app/features/home/review-carousel/review-card/review-card.ts
@@ -13,15 +13,17 @@ import { formatRelativeDate } from '../../../../core/utils/date';
 export class ReviewCard {
   item = input.required<ReviewFeedItemResponse>();
   albumClick = output<number>();
+  reviewClick = output<number>();
   userClick = output<string>();
 
   readonly Star = Star;
   readonly stars = [1, 2, 3, 4, 5];
   readonly formatDate = formatRelativeDate;
 
-  onAlbum(event: Event) {
+  onCard(event: Event) {
     event.stopPropagation();
     this.albumClick.emit(this.item().album.id);
+    this.reviewClick.emit(this.item().id);
   }
 
   onUser(event: Event) {

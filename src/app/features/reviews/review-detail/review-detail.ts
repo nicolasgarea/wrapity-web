74045c992d9af43c7a5d357dfdd684c1616d940b
nicolasgarea <nicogareacidre@gmail.com>
2026-05-26 13:51:09 +0200

    feat(reviews): show more reviews of same album as vertical list

--- src/app/features/reviews/review-detail/review-detail.ts
+++ src/app/features/reviews/review-detail/review-detail.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Location } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -8,12 +8,13 @@ import { ReviewService } from '../../../core/services/review.service';
 import { AuthService } from '../../../core/services/auth.service';
 import { ReviewFeedItemResponse } from '../../../core/models/model/reviewFeedItemResponse';
 import { LikeButton } from '../../../shared/like-button/like-button';
+import { ReviewCard } from '../../home/review-carousel/review-card/review-card';
 import { formatRelativeDate } from '../../../core/utils/date';
 
 @Component({
   selector: 'app-review-detail',
   standalone: true,
-  imports: [RouterLink, LucideAngularModule, LikeButton],
+  imports: [RouterLink, LucideAngularModule, LikeButton, ReviewCard],
   templateUrl: './review-detail.html',
   styleUrl: './review-detail.scss',
 })
@@ -31,6 +32,7 @@ export class ReviewDetail {
 
   loading = signal(true);
   notFound = signal(false);
+  others = signal<ReviewFeedItemResponse[]>([]);
 
   private data = toSignal(
     this.route.paramMap.pipe(
@@ -43,6 +45,7 @@ export class ReviewDetail {
         }
         this.loading.set(true);
         this.notFound.set(false);
+        this.others.set([]);
         return this.reviewService.getById(id).pipe(
           map((review) => {
             this.loading.set(false);
@@ -69,11 +72,30 @@ export class ReviewDetail {
 
   stars = [1, 2, 3, 4, 5];
 
+  constructor() {
+    effect(() => {
+      const r = this.review();
+      if (!r) return;
+      this.reviewService.getByAlbum(r.album.id, 8, 0).subscribe({
+        next: (list) => this.others.set(list.filter((x) => x.id !== r.id)),
+        error: () => this.others.set([]),
+      });
+    });
+  }
+
   goBack(): void {
     if (window.history.length > 1) {
       this.location.back();
     } else {
       this.router.navigate(['/home']);
     }
+  }
+
+  goToReview(reviewId: number) {
+    this.router.navigate(['/reviews', reviewId]);
+  }
+
+  goToUser(username: string) {
+    this.router.navigate(['/users', username]);
   }
 }
